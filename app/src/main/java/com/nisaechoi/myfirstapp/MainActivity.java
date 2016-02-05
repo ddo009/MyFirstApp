8d834926baf44d62e83d94ffca469ad4d27f41de
donghaechoi <power4554>
2016-02-05 09:43:23 +0900

    commit

--- app/src/main/java/com/nisaechoi/myfirstapp/MainActivity.java
+++ app/src/main/java/com/nisaechoi/myfirstapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.nisaechoi.myfirstapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     // AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
         // R은 res폴더랑 연결됨 리소스에 접근시에 R. 을 이용함.
         setContentView(R.layout.practice);
         // onCreate = main 메소드임
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        // 아래랑 위는 동일함
+
+        findViewById(R.id.button1).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button2){
+        Toast.makeText(MainActivity.this, "잘됨", Toast.LENGTH_SHORT).show();
+        }
+        else if (v.getId() == R.id.button1){
+        Toast.makeText(MainActivity.this, "엄청 잘됨", Toast.LENGTH_SHORT).show();
+        }
     }
 }
